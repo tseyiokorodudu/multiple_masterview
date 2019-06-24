b4a2e8827a8987edd95b8f2adc7e68de80d81838
Matthew Landauer <mlandauer@gmail.com>
2019-06-24 12:26:00 +1000

    Now retries failed authorities at the end

--- scraper.rb
+++ scraper.rb
@@ -1,23 +1,35 @@
 #!/usr/bin/env ruby
 Bundler.require
 
-exceptions = {}
-MasterviewScraper::AUTHORITIES.keys.each do |authority_label, params|
-  puts "\nCollecting feed data for #{authority_label}..."
+def scrape(authorities)
+  exceptions = {}
+  authorities.each do |authority_label|
+    puts "\nCollecting feed data for #{authority_label}..."
 
-  begin
-    MasterviewScraper.scrape(authority_label) do |record|
-      record["authority_label"] = authority_label.to_s
-      MasterviewScraper.log(record)
-      ScraperWiki.save_sqlite(["authority_label", "council_reference"], record)
+    begin
+      MasterviewScraper.scrape(authority_label) do |record|
+        record["authority_label"] = authority_label.to_s
+        MasterviewScraper.log(record)
+        ScraperWiki.save_sqlite(["authority_label", "council_reference"], record)
+      end
+    rescue StandardError => e
+      STDERR.puts "#{authority_label}: ERROR: #{e}"
+      STDERR.puts e.backtrace
+      exceptions[authority_label] = e
     end
-  rescue StandardError => e
-    STDERR.puts "#{authority_label}: ERROR: #{e}"
-    STDERR.puts e.backtrace
-    exceptions[authority_label] = e
   end
+  exceptions
+end
+
+exceptions = scrape(MasterviewScraper::AUTHORITIES.keys)
+unless exceptions.empty?
+  puts "\n***************************************************"
+  puts "Now retrying authorities which earlier had failures"
+  puts "***************************************************"
+
+  exceptions = scrape(exceptions.keys)
 end
 
 unless exceptions.empty?
-  raise "There were errors with the following authorities: #{exceptions.keys}. See earlier output for details"
+  puts "There were errors with the following authorities: #{exceptions.keys}. See earlier output for details"
 end
