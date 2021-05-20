4e0e4bd4585ddeaf73e9034cae3b84e33d8ed7cb
Matthew Landauer <mlandauer@gmail.com>
2021-05-20 14:00:38 +1000

    Commenting out brisbane because they are now forcing us to use developmenti

--- lib/masterview_scraper/authorities.rb
+++ lib/masterview_scraper/authorities.rb
@@ -26,11 +26,12 @@ module MasterviewScraper
       use_api: true,
       force_detail: true
     },
-    brisbane: {
-      url: "https://pdonline.brisbane.qld.gov.au/MasterViewUI/Modules/ApplicationMaster",
-      params: { "6" => "F" },
-      force_detail: true
-    },
+    # TODO: They are now redirecting to their developmenti service
+    # brisbane: {
+    #   url: "https://pdonline.brisbane.qld.gov.au/MasterViewUI/Modules/ApplicationMaster",
+    #   params: { "6" => "F" },
+    #   force_detail: true
+    # },
     fairfield: {
       url: "https://openaccess.fairfieldcity.nsw.gov.au/OpenAccess/Modules/Applicationmaster",
       params: { "4a" => 10, "6" => "F" },
